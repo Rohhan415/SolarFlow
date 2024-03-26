dcba639869e7d8b8b08aa5384ec8cc15af6cd788
Kacper Kicki <rohhan.514@gmail.com>
2024-03-26 23:29:49 +0100

    feat: privacy policy

--- src/components/ContactPageSection/ContactForm.tsx
+++ src/components/ContactPageSection/ContactForm.tsx
@@ -19,15 +19,21 @@ function ContactForm() {
   // };
 
   return (
-    <form>
-      <label>Name</label>
-      <input type="text" name="user_name" />
-      <label>Email</label>
-      <input type="email" name="user_email" />
-      <label>Message</label>
-      <textarea name="message" />
-      <input type="submit" value="Send" />
-    </form>
+    <>
+      {" "}
+      <div>
+        <form>
+          <label>Name</label>
+          <input type="text" name="user_name" />
+          <label>Email</label>
+          <input type="email" name="user_email" />
+          <label>Message</label>
+          <textarea name="message" />
+          <input type="submit" value="Send" />
+        </form>
+        accept privacy policy
+      </div>
+    </>
   );
 }
 
