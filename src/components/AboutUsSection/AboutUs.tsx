908821d25f0465b7e0219991a8d97a5e31678b9d
Kacper <rohhan.514@gmail.com>
2024-03-27 23:53:10 +0100

    feat: changed text

--- src/components/AboutUsSection/AboutUs.tsx
+++ src/components/AboutUsSection/AboutUs.tsx
@@ -1,5 +1,16 @@
 function AboutUs() {
-  return <div className="w-full h-96 ">Tutaj będzie sekcja o firmie</div>;
+  return (
+    <div className="w-full h-96 ">
+      Oferujemy kompleksowe usługi związane z energią słoneczną, począwszy od
+      doradztwa i projektowania systemu, poprzez zakup niezbędnych komponentów,
+      aż po montaż i uruchomienie instalacji. W cenie usługi możemy za pomocą
+      drona również przeprowadzić pomiary trzymania ciepła, co pozwala nam
+      dokładnie ocenić efektywność izolacji budynku oraz wykryć ewentualne
+      straty ciepła Korzystamy z usług certyfikowanych specjalistów, którzy
+      posiadają odpowiednie kwalifikacje i doświadczenie w dziedzinie montażu
+      paneli fotowoltaicznych i instalacji elektrycznych.
+    </div>
+  );
 }
 
 export default AboutUs;
