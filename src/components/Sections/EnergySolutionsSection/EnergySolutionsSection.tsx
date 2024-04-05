2e4b34c3a2c9195f709792257efa5da38db9e615
Kacper <rohhan.514@gmail.com>
2024-04-06 01:14:04 +0200

    feat: working on popup

--- src/components/Sections/EnergySolutionsSection/EnergySolutionsSection.tsx
+++ src/components/Sections/EnergySolutionsSection/EnergySolutionsSection.tsx
@@ -1,35 +1,72 @@
 import styles from "./EnergySolutionsSection.module.css";
 import img2 from "../../../assets/image2.jpg";
+import img11 from "../../../assets/image11.jpg";
+import img7 from "../../../assets/image7.jpg";
+import { useState } from "react";
 
 function EnergySolutionsSection() {
+  const [showModal, setShowModal] = useState(false);
+
   return (
     <section className={styles.container}>
       <div className={styles.energySolutions}>
-        <div>
-          {" "}
-          <img className={styles.solutionImage} src={img2} />
+        <div className={styles.solutionContainer}>
+          <div className={styles.imageContainer}>
+            <img className={styles.solutionImage} src={img2} alt="Solution" />
+            <button
+              className={styles.imageButton}
+              onClick={() => setShowModal(true)}
+            >
+              Zobacz więcej
+            </button>
+          </div>
+          {showModal && (
+            <div className={`${styles.popup} ${showModal ? "" : styles.close}`}>
+              <div
+                className={`${styles.popupContent} ${
+                  showModal ? "" : styles.close
+                }`}
+              >
+                <button
+                  className={styles.buttonClose}
+                  onClick={() => setShowModal(false)}
+                >
+                  Close
+                </button>
+              </div>
+            </div>
+          )}
           <div className={styles.solutionDescriptionContainer}>
             <h3 className={styles.solutionTitle}>Fotowoltaika</h3>
             <div className={styles.solutionDescription}>
               Fotowoltaika to technologia, która wykorzystuje energię słoneczną
-              do produkcji elektryczności.Panele mogą być zamontowane na dachu
+              do produkcji elektryczności. Panele mogą być zamontowane na dachu
               domu lub na ziemi co daje szerokie możliwości wykorzystania
               instalacji.
             </div>
           </div>
         </div>
-        <div>
-          {" "}
-          <img className={styles.solutionImage} src={img2} />
+        <div className={styles.solutionContainer}>
+          <img className={styles.solutionImage} src={img11} />
           <div className={styles.solutionDescriptionContainer}>
             <h3 className={styles.solutionTitle}>Pompy Ciepła</h3>
+            <div className={styles.solutionDescription}>
+              Pompy ciepła to urządzenia, które używają energii z otoczenia do
+              ogrzewania lub chłodzenia pomieszczeń. Działają na zasadzie
+              przekazywania ciepła z jednego miejsca do drugiego.
+            </div>
           </div>
         </div>
-        <div>
-          {" "}
-          <img className={styles.solutionImage} src={img2} />
+        <div className={styles.solutionContainer}>
+          <img className={styles.solutionImage} src={img7} />
           <div className={styles.solutionDescriptionContainer}>
             <h3 className={styles.solutionTitle}>Klimatyzacja</h3>
+            <div className={styles.solutionDescription}>
+              Klimatyzacja to system, który reguluje temperaturę, wilgotność i
+              czystość powietrza w pomieszczeniach, zapewniając komfortowe
+              warunki życia. Działa poprzez chłodzenie lub ogrzewanie powietrza
+              oraz usuwanie wilgoci i zanieczyszczeń.
+            </div>
           </div>
         </div>
       </div>
